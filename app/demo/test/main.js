4b5e22b97ce08c262212bc7102f30b91ac984411
wyj <wyj@abbcc.net>
2014-11-12 18:30:21 +0800

    -

--- app/demo/test/main.js
+++ app/demo/test/main.js
@@ -1,16 +1,40 @@
-/**
- * @description main
- * @namespace main
- * @author yongjin on 2014/11/12
- */
+
+// 产品默认值
+var productDefaults = {
+    name: '',
+    type: 'NM',
+    unit: '件',
+    price: 0,
+    sort: 1,
+    loginView: '1',
+    ads: '2',
+    prodtype: '',
+    category: '',
+    isBest: false,
+    isNew: false,
+    photo2: [],
+    photoId: 'Attach_0000000000000000000011056',
+    photo: 'upload/g/g2/ggggfj/picture/2014/09/01/01bcc9d6-4790-403f-a546-eb97fc3aee31.jpg',
+    checked: false
+};
+// 分类默认值
+var categoryDefaults = {
+    grade: '00', // 产品以00开头， 新闻以01开头
+    isroot: '01', // 是否是根目录
+    isdisplay: 1, // 是否显示
+    name: '',
+    sort: 1,
+    state: '01',
+    belongId: null, // 父类ID
+    type: '10' // 产品
+}
+
 
 // 模型类
 var UserModel = Backbone.Model.extend({
-    baseId: 'productId', // 主键标识
-    baseUrl: 'http://jihui88.com/rest/api/user/info', // 请求地址
-    defaults: {
-        name: '未登录'
-    },
+    baseId: 'productId', // 主键标识 [修改]
+    baseUrl: 'http://jihui88.com/rest/api/category/detail', // 请求地址 [修改]
+    defaults: productDefaults, // 默认值 [修改]
     url: function () {
         var base = this.baseUrl;
         if (this.isNew() && !this.id) return base;
@@ -45,30 +69,30 @@ var view = Backbone.View.extend({
     },
     // 保存
     submit: function () {
-        var ctx = this;
-        // 为模型类  赋值
-        $("#J_Form input").each(function () {
-            ctx.model.set($(this).attr('name'), $(this).val());
-        });
-
+        // 设置值
+        this.model.set('name', 'test');
         // 保存模型类
         this.model.save(null, {
             wait: true,
             success: function (response) {
-                console.log(response);
+                console.dir(response);
             }
         });
     },
     // 修改
     edit: function () {
         var ctx = this;
-        this.model.set('id', 'ooooooo1');
+        // 设置ID
+        this.model.set('id', 'Product_000000000000000000116135');
+        // 获取详细信息
         this.model.fetch({
             success: function () {
+                // 设置内容
                 ctx.model.set('name', '222');
+                // 保存内容
                 ctx.model.save(null, {
                     success: function (response) {
-                        alert(response);
+                        console.dir(response);
                     }
                 });
             }
