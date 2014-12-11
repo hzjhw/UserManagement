d03e26a24378a3e7584842a9ce887962da156abf
wyj <wyj@abbcc.net>
2014-12-11 13:09:49 +0800

    wyj

--- app/modules/attributes/config.js
+++ app/modules/attributes/config.js
@@ -3,23 +3,17 @@
  * @namespace config
  * @author yongjin<zjut_wyj@163.com> 2014/12/11
  */
-/*
-attributes: function () {
+app.addRoute('attributes', function () {
   seajs.use(['jquery', 'AttributesList'], function (jquery, AttributesList) {
     app.addView('attributesList', new AttributesList);
   });
-},*/
-
-
-/*
-// attributes
-'AttributesModel': 'models/AttributesModel.js',
-  'AttributesList': 'modules/attributes/controllers/AttributesList.js',
-  'AttributesDetail': 'modules/attributes/controllers/AttributesDetail.js',*/
-/*
-define('template/attributes_item', function (require, exports, module) {
+});
+app.addModule('AttributesDetail', 'modules/attributes/controllers/AttributesDetail.js');
+app.addModule('AttributesList', 'modules/attributes/controllers/AttributesList.js');
+app.addModule('AttributesModel', 'models/AttributesModel.js');
+app.addTemplate('template/attributes_item', function (require, exports, module) {
   module.exports = require('modules/attributes/views/attributes_item.html');
 });
-define('template/attributes_list', function (require, exports, module) {
+app.addTemplate('template/attributes_list', function (require, exports, module) {
   module.exports = require('modules/attributes/views/attributes_list.html');
-});*/
+});
--- app/modules/category/config.js
+++ app/modules/category/config.js
@@ -3,39 +3,30 @@
  * @namespace config
  * @author yongjin<zjut_wyj@163.com> 2014/12/11
  */
-/*
-newsCategory: function () {
+app.addModule('ProductCategoryList', 'modules/category/controllers/ProductCategoryList.js');
+app.addModule('ProductCategoryDetail', 'modules/category/controllers/ProductCategoryDetail.js');
+app.addModule('NewsCategoryList', 'modules/category/controllers/NewsCategoryList.js');
+app.addModule('NewsCategoryDetail', 'modules/category/controllers/NewsCategoryDetail.js');
+app.addModule('CategoryModel', 'models/CategoryModel.js');
+app.addTemplate('template/category_product_item', function (require, exports, module) {
+  module.exports = require('modules/category/views/category_product_item.html');
+});
+app.addTemplate('template/category_product_list', function (require, exports, module) {
+  module.exports = require('modules/category/views/category_product_list.html');
+});
+app.addTemplate('template/category_news_item', function (require, exports, module) {
+  module.exports = require('modules/category/views/category_news_item.html');
+});
+app.addTemplate('template/category_news_list', function (require, exports, module) {
+  module.exports = require('modules/category/views/category_news_list.html');
+});
+app.addRoute('category/news', function () {
   seajs.use(['jquery', 'NewsCategoryList'], function (jquery, NewsCategoryList) {
     app.addView('newsCategoryPage', new NewsCategoryList);
   });
-},*/
-
-
-/*
-productCategory: function () {
+});
+app.addRoute('category/product', function () {
   seajs.use(['jquery', 'ProductCategoryList'], function (jquery, ProductCategoryList) {
     app.addView('productCategoryPage', new ProductCategoryList);
   });
-},*/
-
-/*// category
-'CategoryModel': 'models/CategoryModel.js',
-  'ProductCategoryList': 'modules/category/controllers/ProductCategoryList.js',
-  'ProductCategoryDetail': 'modules/category/controllers/ProductCategoryDetail.js',
-  'NewsCategoryList': 'modules/category/controllers/NewsCategoryList.js',
-  'NewsCategoryDetail': 'modules/category/controllers/NewsCategoryDetail.js',*/
-
-/*
-define('template/category_product_item', function (require, exports, module) {
-  module.exports = require('modules/category/views/category_product_item.html');
 });
-define('template/category_product_list', function (require, exports, module) {
-  module.exports = require('modules/category/views/category_product_list.html');
-});
-define('template/category_news_item', function (require, exports, module) {
-  module.exports = require('modules/category/views/category_news_item.html');
-});
-define('template/category_news_list', function (require, exports, module) {
-  module.exports = require('modules/category/views/category_news_list.html');
-});
-*/
