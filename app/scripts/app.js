e9081b6e4c89a124bdf6cc6dc9cfcafcb351a04a
wyj <wyj@abbcc.net>
2014-12-15 15:40:06 +0800

    wyj

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,10 +5,9 @@ r&&"string"==typeof r.column&&(u=s(i,r.column),c(i,r.column,s(o,r.column)),c(o,r
  * @namespace local
  * @author yongjin<zjut_wyj@163.com> 2014/12/13
  */
-CONST.DEBUG_LOCAL = true
+CONST.LIB_FORDER = 'lib';
 CONST.DEBUG_SEAJS = true;
 CONST.DEBUG_CONSOLE = true;
-CONST.LIB_FORDER = typeof CONST.DEBUG_LOCAL === 'undefined' ? 'lib' : 'base';
 CONST.APP_VERSION = '20141215';
 /**
  * @description config
