3b28bd53ce98a797b73d1fa7abb330015f33d328
dreamoflose <836790625@qq.com>
2014-12-16 09:36:46 +0800

    141216

--- app/modules/member/config.js
+++ app/modules/member/config.js
@@ -3,45 +3,49 @@
  * @namespace config
  * @author yongjin<zjut_wyj@163.com> 2014/12/11
  */
+/**
+ * 模块
+ * */
+app.addModule('MemberModel', 'models/MemberModel.js');
+app.addModule('MemberList', 'modules/member/controllers/MemberList.js');
+app.addModule('MemberDetail', 'modules/member/controllers/MemberDetail.js');
+app.addModule('MemberCategory', 'modules/member/controllers/MemberCategory.js');
+app.addModule('MemberRank', 'modules/member/controllers/MemberRank.js');
 
-/*
-member: function () {
+/**
+ * 路由
+ * */
+app.addRoute('message', function(){
   seajs.use(['jquery', 'MemberCategory'], function (jquery, MemberCategory) {
-    app.addView('memberCategory', new MemberCategory);
+    app.addView('memberCategory', new MemberCategory());
   });
-},*/
-
-
-
-// member
-/*
-'MemberModel': 'models/MemberModel.js',
-  'MemberCategory': 'modules/member/controllers/MemberCategory.js',
-  'MemberList': 'modules/member/controllers/MemberList.js',
-  'MemberRank': 'modules/member/controllers/MemberRank.js',
-  'MemberDetail': 'modules/member/controllers/MemberDetail.js'*/
-
-
-/* member */
-/*
-define('template/member', function (require, exports, module) {
-  module.exports = require('modules/member/member.html');
 });
-define('template/member_list', function (require, exports, module) {
+
+/**
+ * 模板
+ * */
+
+app.addTemplate('template/member', function (require, exports, module) {
+  module.exports = require('modules/member/member_detail.html');
+});
+app.addTemplate('template/member_list', function (require, exports, module) {
   module.exports = require('modules/member/views/member_list.html');
 });
-define('template/member_list_detail', function (require, exports, module) {
+app.addTemplate('template/member_list_detail', function (require, exports, module) {
   module.exports = require('modules/member/views/member_list_detail.html');
 });
-define('template/member_rank', function (require, exports, module) {
+app.addTemplate('template/member_rank', function (require, exports, module) {
   module.exports = require('modules/member/views/member_rank.html');
 });
-define('template/member_rank_detail', function (require, exports, module) {
+app.addTemplate('template/member_rank_detail', function(require, exports, module){
   module.exports = require('modules/member/views/member_rank_detail.html');
 });
-define('template/member_attribute', function (require, exports, module) {
+app.addTemplate('template/member_attribute', function(require, exports, module){
   module.exports = require('modules/member/views/member_attribute.html');
 });
-define('template/member_category', function (require, exports, module) {
+app.addTemplate('template/member_attribute_detail', function(require, exports, module){
+  module.exports = require('modules/member/views/member_attribute_detail.html');
+});
+app.addTemplate('template/member_category', function(require, exports, module){
   module.exports = require('modules/member/views/member_category.html');
-});*/
+});
