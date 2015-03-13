a57c94e1fba7f0f21325da544ad4eef804544948
wyj <wyj@abbcc.net>
2015-03-13 16:16:32 +0800

    wyj

---
+++ app/modules/wwy/leaflet/controllers/LeafletDetail.js
@@ -0,0 +1,10 @@
+/**
+ * @description LeafletDetail
+ * @class LeafletDetail
+ * @author yongjin<zjut_wyj@163.com> 2015/3/13
+ */
+define('LeafletDetail', [], function (require, exports, module) {
+  var LeafletDetail;
+
+  module.exports = LeafletDetail;
+});
