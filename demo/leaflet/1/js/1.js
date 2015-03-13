ec2cbc24b115c65985ce1430bf75a14e1b5e1e71
Administrator <zjut_wyj@163.com>
2015-03-13 17:27:34 +0800

    wxw

---
+++ demo/leaflet/1/js/1.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 15-3-13.
+ */
+$(document).ready(function(){
+    $("#section21 .wrapper .rotate").after("<div class='zp_txt'><p><span>1、</span> 15058****26抽中2等奖：500元抵金券</p><p><span>2、</span> 15867****45抽中2等奖：500元抵金卷</p><p><span>3、</span> 15867****21抽中3等奖：100元抵金券</p><p><span>4、</span> 18058****92抽中3等奖：100元抵金券</p><p><span>5、</span> 13645****48抽中3等奖：100元抵金券</p><p>抵金券不兑现，购买商品可抵用，不找零<p></div>");
+});
